048169377e63887c9aacc6de55e34f53737fb1d1
ItaloSLeao <italoleao200656@gmail.com>
2026-03-26 07:29:32 -0300

    fix malicious_request

--- waf.rb
+++ waf.rb
@@ -8,7 +8,7 @@ class WAF
     /UNION\s+SELECT/i,            # sql injection — extracao de dados
     /DROP\s+TABLE/i,              # sql injection — destruicao de dados
     /INSERT\s+INTO/i,             # sql injection — insercao maliciosa
-    /\/..+/,                      # path traversal — /../
+    /\/(\.\.\/)+/,                      # path traversal — /../
     /<[a-z]+[^>]*>/i,             # xss — qualquer tag HTML
     /javascript:/i,               # xss — protocolo javascript
     /on\w+\s*=/i,                 # xss — event handlers (onclick=, onload=, etc)
@@ -16,6 +16,18 @@ class WAF
 
   def malicious_request?(path)
     decode = URI.decode_www_form_component(path)
+
+    if decode =~ /^https?:\/\// #se o caminho for uma url completa
+      begin
+        uri = URI.parse(decode) #extrai o caminho
+        path_check = uri.path
+      rescue URI::InavalidURIError
+        return true #url invalida, malicioso
+      end
+    else
+      path_check = decode
+    end
+
     ATTACKING_PATTERNS.any? { |pattern| decode.match?(pattern) } #retorna true se algum padrao de ataque for identificado
   end
 
